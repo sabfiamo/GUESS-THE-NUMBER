87814a16663b14d64ba79c445d532c1a11e9f911
sabfiamo <sabfiamo@gmail.com>
2023-02-14 10:55:41 +0100

    improve code in file .js

--- js/main.js
+++ js/main.js
@@ -1,16 +1,20 @@
 'use strict'
-const button=document.querySelector('.js_button');
+const buttonTest=document.querySelector('.js_button');
 const inputElement=document.querySelector('.js_input');
 const infoElement=document.querySelector('.js_info');
 const countElement=document.querySelector('.js_count');
+//contador de intentos
+let timesCounter=0;
 
 function getRandomNumber(max)  
 {
   return (Math.ceil(Math.random() * max));
 }
+//número aleatorio entre 1 y 100
+const numberRandom=getRandomNumber(100)+1;
 
-
-function handleClickButton(event){
+function handleClickButton(event)
+{
     event.preventDefault();
     timesCounter++;
     console.log('Número introducido: '+inputElement.value);
@@ -19,14 +23,17 @@ function handleClickButton(event){
 
     //Contar los intentos
     countElement.innerHTML='Número de intentos: ' +timesCounter;
+
     const inputNumber=parseInt(inputElement.value);
+
     if ((inputNumber>100) || (inputNumber<0))
     {
         infoElement.innerHTML="Pista: El número debe estar entre 1 y 100";
-    }else if (inputNumber===numberRandom )
-        {
-            infoElement.innerHTML="Has ganado campeona!!!";
-        }
+    }
+    else if (inputNumber===numberRandom )
+    {
+        infoElement.innerHTML="Has ganado campeona!!!";
+    }
     else if (inputNumber>numberRandom )
     {
         infoElement.innerHTML="Pista: Demasiado alto.";
@@ -37,8 +44,5 @@ function handleClickButton(event){
     }
     
 }
-//número aleatorio entre 1 y 100
-const numberRandom=getRandomNumber(100)+1;
-//contador de intentos
-let timesCounter=0;
-button.addEventListener('click', handleClickButton);
+
+buttonTest.addEventListener('click', handleClickButton);
