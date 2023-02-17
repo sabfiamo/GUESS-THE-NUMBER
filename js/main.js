413139519db1fee6d81f8d1f86838d030cb80bc6
sabfiamo <sabfiamo@gmail.com>
2023-02-17 14:24:45 +0100

    Extract the feedback painter to a function.

--- js/main.js
+++ js/main.js
@@ -3,6 +3,8 @@ const buttonTest=document.querySelector('.js_button');
 const inputElement=document.querySelector('.js_input');
 const infoElement=document.querySelector('.js_info');
 const countElement=document.querySelector('.js_count');
+//número aleatorio entre 1 y 100
+const numberRandom=getRandomNumber(100)+1;
 //contador de intentos
 let timesCounter=0;
 
@@ -10,40 +12,47 @@ function getRandomNumber(max)
 {
   return (Math.ceil(Math.random() * max));
 }
-//número aleatorio entre 1 y 100
-const numberRandom=getRandomNumber(100)+1;
 
-function handleClickButton(event)
+function writeMsj(message)
 {
-    event.preventDefault();
-    timesCounter++;
-    console.log('Número introducido: '+inputElement.value);
-    console.log('Número aleatorio: '+numberRandom);
-    console.log('Contador de intentos:'+timesCounter);
+    infoElement.innerHTML=message;
+}
 
-    //Contar los intentos
-    countElement.innerHTML='Número de intentos: ' +timesCounter;
-
+function compareNumbers()
+{
     const inputNumber=parseInt(inputElement.value);
 
     if ((inputNumber>100) || (inputNumber<0))
     {
-        infoElement.innerHTML="Pista: El número debe estar entre 1 y 100";
+        writeMsj("Pista: El número debe estar entre 1 y 100");
     }
     else if (inputNumber===numberRandom )
     {
-        infoElement.innerHTML="Has ganado campeona!!!";
+        writeMsj("Has ganado campeona!!!");
         setInterval("location.reload()",2000);
 
     }
     else if (inputNumber>numberRandom )
     {
-        infoElement.innerHTML="Pista: Demasiado alto.";
+        writeMsj("Pista: Demasiado alto.");
     }
     else
     {
-        infoElement.innerHTML="Pista: Demasiado bajo.";
+        writeMsj("Pista: Demasiado bajo.");
     }
+    
+}
+
+function handleClickButton(event)
+{
+    event.preventDefault();
+    timesCounter++;
+    console.log('Número introducido: '+ inputElement.value);
+    console.log('Número aleatorio: '+ numberRandom);
+    console.log('Contador de intentos:'+ timesCounter);
+    //Contar los intentos
+    countElement.innerHTML='Número de intentos: ' + timesCounter;
+    compareNumbers();
     
 }
 
