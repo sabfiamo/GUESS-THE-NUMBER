3157d443aea425d240a3bdf508f3d4e72346f8ed
sabfiamo <sabfiamo@gmail.com>
2023-05-02 23:48:27 +0200

    add run docs

--- js/main.js
+++ js/main.js
@@ -1,68 +1,64 @@
-'use strict'
-const buttonTest=document.querySelector('.js_button');
-const inputElement=document.querySelector('.js_input');
-const infoElement=document.querySelector('.js_info');
-const countElement=document.querySelector('.js_count');
+"use strict";
+const inputElement = document.querySelector(".js_input");
+const infoElement = document.querySelector(".js_info");
+const countElement = document.querySelector(".js_count");
+const buttonTable = document.querySelector(".js_buttonTable");
+
 //número aleatorio entre 1 y 100
-const numberRandom=getRandomNumber(100)+1;
+const numberRandom = getRandomNumber(100) + 1;
 //contador de intentos
-let timesCounter=0;
+let timesCounter = 0;
 
-function getRandomNumber(max)  
-{
-  return (Math.ceil(Math.random() * max));
+function getRandomNumber(max) {
+  return Math.ceil(Math.random() * max);
 }
 
-function writeMsj(message)
-{
-    infoElement.innerHTML=message;
+function writeMsj(message) {
+  infoElement.innerHTML = message;
 }
-function checkValue()
-{
-    const valoresAceptados = /^[0-9]+$/;
-     if ((!(inputElement.value===''))&&
-    (inputElement.value.match(valoresAceptados)))
-    {
-        return true;
-    }
-    else return false;
+function checkValue() {
+  const valoresAceptados = /^[0-9]+$/;
+  if (
+    !(inputElement.value === "") &&
+    inputElement.value.match(valoresAceptados)
+  ) {
+    return true;
+  } else return false;
 }
-function compareNumbers()
-{
-    const inputNumber=parseInt(inputElement.value);
+function compareNumbers() {
+  const inputNumber = parseInt(inputElement.value);
 
-    if ((inputNumber>100) || (inputNumber<0))
-    {
-        writeMsj("Pista: El número debe estar entre 1 y 100");
-    }
-    else if (inputNumber===numberRandom )
-    {
-        writeMsj("Has ganado campeona!!!");
-        setInterval("location.reload()",2000);
-
-    }
-    else if (inputNumber>numberRandom )
-    {
-        writeMsj("Pista: Demasiado alto.");
-    }
-    else
-    {
-        writeMsj("Pista: Demasiado bajo.");
-    }
-    
+  if (inputNumber > 100 || inputNumber < 0) {
+    writeMsj("Pista: El número debe estar entre 1 y 100");
+  } else if (inputNumber === numberRandom) {
+    writeMsj("Congratulations!! You got it!!");
+    setInterval("location.reload()", 3000);
+  } else if (inputNumber > numberRandom) {
+    writeMsj(`Hint: The ${inputNumber} is too high.`);
+  } else {
+    writeMsj(`Hint: The ${inputNumber} is too low.`);
+  }
 }
 
-function handleClickButton(event)
-{
-    event.preventDefault();
-     //Contar los intentos
-    
-    if (checkValue())
-    {
-        timesCounter++;
-        countElement.innerHTML='Número de intentos: ' + timesCounter;
-        compareNumbers();
-    }     
+function handleClickButtonTable(event) {
+  event.preventDefault();
+  //Contar los intentos
+  inputElement.style.background = "none";
+  if (event.target.textContent === "") {
+    inputElement.value = inputElement.value.slice(0, -1);
+  } else if (event.target.textContent === "Reset") {
+    location.reload();
+  } else if (event.target.textContent === "Guess") {
+    if (checkValue()) {
+      timesCounter++;
+      countElement.innerHTML = "Number of attemps: " + timesCounter;
+      compareNumbers();
+    } else writeMsj("Hint: The number has to have a value between 1 and 100.");
+    // inputNumberElement.value = "";
+    inputElement.value = "";
+  } else {
+    inputElement.value = inputElement.value + event.target.textContent;
+  }
 }
 
-buttonTest.addEventListener('click', handleClickButton);
+buttonTable.addEventListener("click", handleClickButtonTable);
